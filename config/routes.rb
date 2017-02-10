6d89cc707ce23678df77dc92deab34a6b5738320
avandeloo <anthony.vandeloo@gmail.com>
2017-02-10 14:59:37 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get '/employees' => 'employees#index'
+  post '/employees' => 'employees#create'
+  get '/employees/:id' => 'employees#show'
+  patch '/employees/:id' => 'employees#update'
+  delete '/employees/:id' => 'employees#destroy'
+
+end
